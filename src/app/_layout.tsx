40774be7dca800e6e66dd67c912a81011deb0f11
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-03-10 22:59:29 -0300

    Segundo commit
    Configurando e criando as pastas e arquivos.

---
+++ src/app/_layout.tsx
@@ -0,0 +1,16 @@
+import "@/styles/global.css"
+
+import { Slot } from "expo-router"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+export default function Layout(){
+    return(
+        <GestureHandlerRootView>
+            <Slot />
+        </GestureHandlerRootView>
+    )
+
+}
+
+
+    
