f9cab445deac3a4c865a970847a9466700f280eb
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-03-14 22:20:59 -0300

    Oitavo commit
    Criando o menu inferior.

---
+++ src/components/drawer-content.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import { DrawerContentComponentProps } from "@react-navigation/drawer";
+
+export function DrawerContent(drawerProps: DrawerContentComponentProps){
+    return(
+        <View></View>
+    )
+}
